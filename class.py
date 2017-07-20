0da889e41ca50722a96285a78a29c6daf0e32ae4
Exsodon <Radunciko@gmail.com>
2017-07-21 00:25:02 +0200

    Create class.py

---
+++ class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+class Fecha():
+	def __init__(self):
+		self.__dia = 1
+		
+	def getDia(self, dia):
+		return self.__dia
+		
+	def setDia(self, dia):
+		if dia > 0 and dia < 31:
+			self.__dia = dia
+		else:
+			print "Error"
+		
+mi_fecha = Fecha()
+mi_fecha.setDia(33)
