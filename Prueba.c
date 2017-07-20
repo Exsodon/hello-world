0e1acde25912e1379c44094c390da4ee79f9a9a7
Exsodon <Radunciko@gmail.com>
2017-07-21 00:22:00 +0200

    Update Prueba.c

--- Prueba.c
+++ Prueba.c
@@ -1,1 +1,19 @@
 # include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+int main(void) {
+  
+  int x, y, total;
+  
+  printf("Cual es el valor de lar dos variables: \n");
+  scanf("%i %i",&x, &y);
+  fflush(stdin);
+  total = x + y;
+  
+  printf("El valor de las dos variables es: %i \n", total);
+  fflush(stdin);
+  getchar();
+  
+  return 0;
+}
